ff74e0d7055568a5327d7bba443e45b324f9ef9a
Tenko Mirev <t.d.mirev@gmail.com>
2025-10-22 18:27:52 +0100

    Add files via upload
    This project uses the Python Turtle module to simulate a colourful floral design through procedural graphics. It imitates a flower which is made of 18 curved petals, and the entire design rotates and shifts with each iteration.

---
+++ Shape.py
@@ -0,0 +1,22 @@
+from turtle import *
+import colorsys
+
+speed(11)
+bgcolor("black")
+h = 0
+pensize(4)
+width(8)
+
+for i in range (50):
+    for j in range(18):
+        c = colorsys.hsv_to_rgb(h, 1, 1)
+        color(c)
+        h += 0.005
+        rt(90)
+        circle(150 - j * 6, 90)
+        lt(90)
+        circle(150 - j * 6, 90)
+        rt(180)
+        circle(40, 24)
+
+done()
